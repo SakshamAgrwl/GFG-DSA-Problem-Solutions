d196f79506052648b3c5efc2ee51d9cbc9db6244
Pratheek R. Pai <paiastro1709@gmail.com>
2020-10-04 20:19:06 +0530

    organized
    finding gcd of two numbers using both naive and fast algorithms

---
+++ Mathematics/gcd_fast_and_naive.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int gcd_naive(int a, int b) {
+  int current_gcd = 1;
+  for (int d = 2; d <= a && d <= b; d++) {
+    if (a % d == 0 && b % d == 0) {
+      if (d > current_gcd) {
+        current_gcd = d;
+      }
+    }
+  }
+  return current_gcd;
+}
+
+int gcd_fast(int a, int b) {
+  int m, n, rem;
+  m = a;
+  n = b;
+
+  while (n != 0) {
+        rem  = m % n;
+        m = n;
+        n = rem;
+  }
+ return m;
+}
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  //std::cout << gcd_naive(a, b) << std::endl;
+  std::cout << gcd_fast(a, b) << std::endl;
+  return 0;
+}
