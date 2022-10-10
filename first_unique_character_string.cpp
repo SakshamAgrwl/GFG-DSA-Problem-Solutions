79001f6041f922ef6fed36bd9a76bf29a0d63766
Saksham Agarwal <saksham.agarwal@bennett.edu.in>
2022-10-10 23:45:24 +0530

    Added hashing string problem solutions

---
+++ first_unique_character_string.cpp
@@ -0,0 +1,20 @@
+//Probelm link: https://leetcode.com/problems/first-unique-character-in-a-string/
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        unordered_map<char,int> mp;
+        
+        for(auto it: s){
+            mp[it]++;
+        }
+        
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
